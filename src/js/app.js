dcf54597c5470c3c7dae14f20d34eb1eabec81c1
AmiraMandi <amiramandisbaa@gmail.com>
2023-01-16 20:24:54 +0000

    conditional profile card

--- src/js/app.js
+++ src/js/app.js
@@ -28,19 +28,58 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  let twitter = " ";
+  if (variables.twitter) {
+    twitter =
+      '<a href="https://twitter.com/' +
+      variables.twitter +
+      ' " target= "blank"><i class="fab fa-twitter"></i></a>';
+  } else {
+    twitter = '<a href="#"><i class="fab fa-twitter"></i></a>';
+  }
+
+  let linkedin = " ";
+  if (variables.linkedin) {
+    linkedin =
+      '<a href="https://www.linkedin.com/in/' +
+      variables.linkedin +
+      ' " target= "blank"><i class="fab fa-linkedin"></i></a>';
+  } else {
+    linkedin = '<a href="#"><i class="fab fa-linkedin"></i></a>';
+  }
+
+  let github = " ";
+  if (variables.github) {
+    github =
+      '<a href="https://github.com/' +
+      variables.github +
+      ' " target= "blank"><i class="fab fa-github"></i></a>';
+  } else {
+    github = '<a href="#"><i class="fab fa-github"></i></a>';
+  }
+
+  let instagram = " ";
+  if (variables.instagram) {
+    instagram =
+      '<a href="https://www.instagram.com/amira.mandi/' +
+      variables.instagram +
+      ' " target= "blank"><i class="fab fa-instagram"></i></a>';
+  } else {
+    instagram = '<a href="#"><i class="fab fa-instagram"></i></a>';
+  }
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name ? variables.name : " "} ${ variables.lastname ? variables.lastname : ""}</h1>
+          <h2>${variables.role ? variables.role : ""}</h2>
+          <h3>${variables.city ? variables.city : ""} ${variables.country ? variables.country : ""}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li>${twitter}</li>
+            <li>${github}</li>
+            <li>${linkedin}</li>
+            <li>${instagram}</li>
           </ul>
         </div>
     `;
@@ -56,12 +95,12 @@ window.onload = function() {
     // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "https://randomuser.me/api/portraits/lego/6.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: null,
     linkedin: null,
     instagram: null,
     name: null,
